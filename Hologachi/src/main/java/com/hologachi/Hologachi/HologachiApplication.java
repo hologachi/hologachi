08ca10b69172c9211c33cf77c84f5026c1f06cc3
Hyejin Eom <yazbyz@gmail.com>
2021-06-27 22:10:13 +0900

    [ADD] Spring Boot 프로젝트 생성
    Spring Web, JPA, MySQL, Lombok, Spring Boot DevTools

---
+++ Hologachi/src/main/java/com/hologachi/Hologachi/HologachiApplication.java
@@ -0,0 +1,13 @@
+package com.hologachi.Hologachi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HologachiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HologachiApplication.class, args);
+	}
+
+}
---
+++ Hologachi/src/test/java/com/hologachi/Hologachi/HologachiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hologachi.Hologachi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HologachiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
