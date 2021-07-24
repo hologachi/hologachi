5d0114ca6985a759fe46f9560a10a9cc3915bd80
seonae <sunellie55@gmail.com>
2021-07-25 00:20:00 +0900

    [박선애]fork 해서 구현했던 것들 feature branch 새로 만듦, 메인 화면 옮김

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import '../src/css/App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import '../src/css/App.css';
+import  Home  from "./js/main/home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          {/* Mypage */}
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/js/main/home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+// import Feed from "./feed"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./header"
+// import Post from "./post"
+import Footer from "./footer"
+
+function home() {
+    return (
+        <div className="Home">
+            <Header />
+            <div className="home__body">
+                {/* <Feed />
+                <Post /> */}
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default home
