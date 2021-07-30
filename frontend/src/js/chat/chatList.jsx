50e022e9f84ee11340906536cf5837dbe397201c
Hyejin Eom <yazbyz@gmail.com>
2021-07-31 00:28:07 +0900

    [UPDATE] 채팅방 목록 조회

--- frontend/src/js/chat/chatList.jsx
+++ frontend/src/js/chat/chatList.jsx
@@ -4,18 +4,82 @@ import Header from "../main/header";
 import Footer from "../main/footer";
 import ChatRoom from "./chatRoom";
 import ChatMessage from "./chatMessage";
-import ChatMessageCover from "./chatMessageCover";
 import 'bootstrap/dist/css/bootstrap.min.css';
+//chat
+import ChatRoomService from '../services/ChatRoomService';
+
+import socketClient from "socket.io-client";
+const SERVER = "http://127.0.0.1:8088";
+
 
 class ChatList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {isChatRoomClicked: false};
-        this.handleChatRoomClicked = this.handleChatRoomClicked.bind(this);
+
+    // chat
+    state = {
+        chatrooms: null,
+        socket: null,
+        chatroom: null
+    }
+    
+    socket;
+    
+    componentDidMount() {
+        this.loadChatrooms();
+        this.configureSocket();
+    }
+    
+    loadChatrooms = () => {
+        // 참여하고 있는 채팅방 id 목록 가져오기 
+        ChatRoomService.getChatRoomList().then((res) => {
+            this.setState({ chatrooms: res.data });
+        })
     }
 
-    handleChatRoomClicked = () => {
-        this.setState({isChatRoomClicked: true});
+    configureSocket = () => {
+        var socket = socketClient(SERVER);
+        socket.on('connection', () => {
+            if (this.state.chatroom) {
+                this.handleChannelSelect(this.state.chatroom.chatroom_id);
+            }
+        });
+        socket.on('channel', channel => {
+            
+            let channels = this.state.chatrooms;
+            channels.forEach(c => {
+                if (c.id === channel.id) {
+                    c.participants = channel.participants;
+                }
+            });
+            this.setState({ channels });
+        });
+        socket.on('message', message => {
+            
+            let channels = this.state.chatrooms
+            channels.forEach(c => {
+                if (c.id === message.channel_id) {
+                    if (!c.messages) {
+                        c.messages = [message];
+                    } else {
+                        c.messages.push(message);
+                    }
+                }
+            });
+            this.setState({ channels });
+        });
+        this.socket = socket;
+    }
+
+    handleChatroomSelect = chatroom_id => {
+        let channel = this.state.chatrooms.find(c => {
+            return c.chatroom_id === chatroom_id;
+        });
+        this.setState({ channel });
+        this.socket.emit('channel-join', chatroom_id, ack => {
+        });
+    }
+
+    handleSendMessage = (chatroom_id, text) => {
+        this.socket.emit('send-message', { chatroom_id, text, senderName: this.socket.id, id: Date.now() });
     }
 
     render() {
@@ -24,9 +88,9 @@ class ChatList extends Component {
         let chatmessage = null;
 
         if(isChatRoomClicked) {
-            chatmessage = <ChatMessage />;
+            chatmessage = <ChatMessage onSendMessage={this.handleSendMessage} channel={this.state.chatroom} />;
         } else {
-            chatmessage = <ChatMessageCover />
+            chatmessage = <p>채팅방을 눌러보세요.</p>;
         }
 
         return (
@@ -34,7 +98,7 @@ class ChatList extends Component {
                 <Header />
                 
                 <div className="chatList">
-                    <ChatRoom parentsFunc={this.handleChatRoomClicked}/>
+                    <ChatRoom chatrooms={this.state.chatrooms} onSelectChatroom={this.handleChatroomSelect} />
                     {chatmessage}
                 </div>
                 <Footer />
--- frontend/src/js/chat/room.jsx
+++ frontend/src/js/chat/room.jsx
@@ -1,34 +1,23 @@
 import '../../css/room.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
-import ChatRoomService from '../services/ChatRoomService';
 
 class Room extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-                chatrooms: []
-        }
-    }
-
-    componentDidMount(){
-        ChatRoomService.getChatRoomList().then((res) => {
-            this.setState({ chatrooms: res.data });
-        });
+    click = () => {
+        this.props.onClick(this.props.chatroom_id);
+        
+        console.log(this.props.chatroom_id);
     }
 
     render() {
         return (
             <div className="roomList">
-                {this.state.chatrooms.map(
-                    chatroom => 
-                    <div className="room" key="{chatroom.chatroom_id}" onClick={() => alert('채팅방 입장')}>
-                        <img className="roomImg" src="https://placeimg.com/50/50/any" alt="" />
-                        <span className="roomName">{chatroom.room_name}</span>
-                        <span className="roomLastChat">{chatroom.lastchat}</span>
-                    </div>
-                )}
+                <div className="room" key="{this.props.chatroom_id}" onClick={this.click}>
+                    <img className="roomImg" src="https://placeimg.com/50/50/any" alt="" />
+                    <span className="roomName">{this.props.room_name}</span>
+                    <span className="roomLastChat">{this.props.lastchat}</span>
+                </div>
             </div>
         )
     }
