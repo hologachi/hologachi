4aa3cb6a0d5fceb1e4d3762e8a0da94bb507b907
seonae <sunellie55@gmail.com>
2021-08-25 12:12:51 +0900

    내가 작성한 글 프론트, 백 연결

--- frontend/src/js/mypage/mypageside.js
+++ frontend/src/js/mypage/mypageside.js
@@ -5,11 +5,11 @@ function mypageside() {
     return (
         <div>
             <div id="menu">
-              <a href="./bookmark" className="titleList">- 북마크</a><br />
-              <a href="./mywriting" className="titleList">- 작성한 글</a><br />
-              <a href="./applygb" className="titleList">- 신청한 글</a><br />
-              <a href="./commentwrite" className="titleList">- 댓글 남긴 글</a><br />
-              <a href="#" className="titleList">- 개인정보 수정</a><br />
+              <a href="./bookmark" className="titleList">북마크</a><br />
+              <a href="./mywriting" className="titleList">작성한 글</a><br />
+              <a href="./applygb" className="titleList">신청한 글</a><br />
+              <a href="./commentwrite" className="titleList">댓글 남긴 글</a><br />
+              <a href="#" className="titleList">개인정보 수정</a><br />
             </div>
         </div>
     )
