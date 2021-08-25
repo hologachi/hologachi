eeea542735a0141ec5b16d7ae250cb181becf0b7
Hyejin Eom <yazbyz@gmail.com>
2021-08-25 18:36:30 +0900

    [UPDATE] 공동구매 글 @JsonFormat 설정(yyyy-MM-dd)

--- backend/src/main/java/com/hologachi/backend/model/Post.java
+++ backend/src/main/java/com/hologachi/backend/model/Post.java
@@ -9,6 +9,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,11 +27,14 @@ public class Post {
 	@ManyToOne
 	@JoinColumn(name="sgster_id")
 	private User user;
+	@JsonFormat(pattern="yyyy-MM-dd")
 	private Date rgst_at;
+	@JsonFormat(pattern="yyyy-MM-dd")
 	private Date update_at;
 	private String title;
 	private String content;
 	private int matching;
+	@JsonFormat(pattern="yyyy-MM-dd")
 	private Date deadline;
 	private int deleted_by;
 	
