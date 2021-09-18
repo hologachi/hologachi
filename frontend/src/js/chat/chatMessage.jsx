c8716710b355fd5e35b1fb303d29b552473eb921
Hyejin Eom <yazbyz@gmail.com>
2021-09-18 20:04:38 +0900

    [UPDATE] 채팅 기능 완료

--- frontend/src/js/chat/chatMessage.jsx
+++ frontend/src/js/chat/chatMessage.jsx
@@ -1,11 +1,11 @@
 import '../../css/chat.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { Component } from 'react';
+import React from 'react';
 import Message from './message';
 
-class ChatMessage extends Component {
+const ChatMessage = (props) => {
 
-    checkMessageOwn = (sender) => {
+    const checkMessageOwn = (sender) => {
         if(sessionStorage.getItem('nickname') === sender) {
             return true;
         } else { 
@@ -13,15 +13,14 @@ class ChatMessage extends Component {
         }
     }
 
-    render() {
-
-        return (
-            <div className="chatMessageTop">
-                {this.props.messages.map((message, i) => <div key={i}><Message own={this.checkMessageOwn(message.sender)} sendAt={message.date} message={message.message} sender={message.sender} /></div>)}
-            </div>
-        )
-    }
+    return (
+        <div className="chatMessageTop">
+            { (props.messages.length == 0) && <p>이전에 나눈 메세지가 없습니다.</p>} 
+            { props.messages && props.messages.map((message, i) => <div key={i}><Message own={checkMessageOwn(message.sender)} sendAt={message.date} message={message.message} sender={message.sender} /></div>)}
+            
+        </div>
+    )
 }
 
-export default ChatMessage
+export default ChatMessage;
 
--- frontend/src/js/chat/message.jsx
+++ frontend/src/js/chat/message.jsx
@@ -1,26 +1,25 @@
 import '../../css/message.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { Component } from 'react';
+import React from 'react';
 
-class Message extends Component {
-    render() {
-        const own = this.props.own
+const Message = (props) => {
+    
+    const own = props.own;
 
-        return (
-            <div className={own ? "message own" : "message"}>
-                <div className="message">
-                    <div className="messageTop">
-                        <img className="messageImg" src="https://placeimg.com/50/50/any" alt="" />
-                        <div className="messageData">
-                            <span className="messageSender">{this.props.sender}</span>
-                            <p className="messageText">{this.props.message}</p>
-                            <span className="messageSendAt">{this.props.sendAt}에 전송</span>
+    return (
+        <div className={own ? "message own" : "message"}>
+            <div className="message">
+                <div className="messageTop">
+                    <img className="messageImg" src="https://placeimg.com/50/50/any" alt="" />
+                    <div className="messageData">
+                            <span className="messageSender">{props.sender}</span>
+                            <p className="messageText">{props.message}</p>
+                            <span className="messageSendAt">{props.sendAt}에 전송</span>
                         </div>    
-                    </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
-export default Message
+export default Message;
